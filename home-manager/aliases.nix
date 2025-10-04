96c65099f9b31f924d00ae15df2c624d53dc0b80
Nick Lundin <nick@moontower.net>
2025-10-03 21:33:04 -0500

    Added alias for nix switch

--- home-manager/aliases.nix
+++ home-manager/aliases.nix
@@ -1,5 +1,6 @@
 {
   home.shellAliases = {
+    nxs = "git add . && sudo nixos-rebuild switch --flake .";
     hms = "git add . && home-manager switch --flake .";
     tux = "tmuxp load .";
     ns = "echo \"enter search term\"; read variable_name; echo \"$variable_name\" | xargs nix-search {} --json | jq -s | jq '.[].package_pname' | fzf --bind 'enter:become(nix-shell -p {})'";
