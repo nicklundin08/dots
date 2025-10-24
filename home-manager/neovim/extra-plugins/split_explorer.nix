74b81a58300b98b715a3fdbf6684955cbf880d5a
Nick Lundin <nick@moontower.net>
2025-10-23 19:18:18 -0600

    Oil instead of netrw

--- home-manager/neovim/extra-plugins/split_explorer.nix
+++ home-manager/neovim/extra-plugins/split_explorer.nix
@@ -3,37 +3,37 @@
     #############################################
     # NetRW
     #############################################
-    extraPlugins = [
-      (pkgs.vimUtils.buildVimPlugin {
-        name = "netrw.nvim";
-        src = pkgs.fetchFromGitHub {
-          owner = "prichrd";
-          repo = "netrw.nvim";
-          rev = "90501c62b9b816ed3ed1e912ae9db9af5671a1b2";
-          hash = "sha256-tEO+omzlaC0uFkIHebeqnGtiPbDelRTBJONEnTy0F9U=";
-        };
-      })
-    ];
+    # extraPlugins = [
+    #   (pkgs.vimUtils.buildVimPlugin {
+    #     name = "netrw.nvim";
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "prichrd";
+    #       repo = "netrw.nvim";
+    #       rev = "90501c62b9b816ed3ed1e912ae9db9af5671a1b2";
+    #       hash = "sha256-tEO+omzlaC0uFkIHebeqnGtiPbDelRTBJONEnTy0F9U=";
+    #     };
+    #   })
+    # ];
 
-    extraConfigLua = ''
-      require("netrw").setup({});
+    # extraConfigLua = ''
+    #   require("netrw").setup({});
 
-      vim.api.nvim_create_autocmd('FileType', {
-      	pattern = 'netrw',
-      	desc = 'Remap create file in netrw',
-      	callback = function()
-      		vim.keymap.set('n', 'n', '%', { remap = true, buffer = true })
-      	end
-      });
-    '';
+    #   vim.api.nvim_create_autocmd('FileType', {
+    #   	pattern = 'netrw',
+    #   	desc = 'Remap create file in netrw',
+    #   	callback = function()
+    #   		vim.keymap.set('n', 'n', '%', { remap = true, buffer = true })
+    #   	end
+    #   });
+    # '';
 
-    keymaps = [
-      {
-        action = ":Ex<CR>";
-        key = "-";
-        mode = "n";
-      }
-    ];
+    # keymaps = [
+    #   {
+    #     action = ":Ex<CR>";
+    #     key = "-";
+    #     mode = "n";
+    #   }
+    # ];
 
     #############################################
     # Yazi
@@ -59,21 +59,26 @@
     # Oil
     #############################################
 
-    # plugins = {
-    #   oil = {
-    #     enable = true;
-    #     settings.view_options.show_hidden = true;
-    #     settings.skip_confirm_for_simple_edits = true;
-    #   };
-    # };
+    plugins = {
+      oil = {
+        enable = true;
+        settings.view_options.show_hidden = true;
+        settings.skip_confirm_for_simple_edits = true;
+        settings.promt_save_on_select_new_entry = true;
+        settings.watch_for_changes = true;
+        settings.keymaps = {
+          "<C-s>" = false;
+        };
+      };
+    };
 
-    # keymaps = [
-    #   {
-    #     mode = "n";
-    #     key = "-";
-    #     options.silent = true;
-    #     action = "<CMD>Oil<CR>";
-    #   }
-    # ];
+    keymaps = [
+      {
+        mode = "n";
+        key = "-";
+        options.silent = true;
+        action = "<CMD>Oil<CR>";
+      }
+    ];
   };
 }
