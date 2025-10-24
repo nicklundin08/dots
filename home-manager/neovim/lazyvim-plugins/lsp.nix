2409016f76ee786cfc64d3a4361eb2765051fc1d
Nick Lundin <nick@moontower.net>
2025-10-23 20:14:27 -0600

    Add some LSP stuff

--- home-manager/neovim/lazyvim-plugins/lsp.nix
+++ home-manager/neovim/lazyvim-plugins/lsp.nix
@@ -7,6 +7,23 @@
   programs.nixvim.plugins = {
     lsp = {
       enable = true;
+      inlayHints = true;
+      keymaps = {
+        extra = [
+          {
+            key = "gd";
+            action.__raw = "require('telescope.builtin').lsp_definitions";
+          }
+          {
+            key = "ca";
+            action = "vim.lsp.buf.code_action()";
+          }
+          # {
+          #   key = "cd";
+          #   action = "vim.lsp.diagnostic()";
+          # }
+        ];
+      };
     };
   };
 }
