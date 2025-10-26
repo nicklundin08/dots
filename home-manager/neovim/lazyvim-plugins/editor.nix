082361aecbf8b9dfa79499af9ef2500a227dda39
Nick Lundin <nicholas.lundin@betterlesson.com>
2025-10-26 16:11:25 -0600

    Default lazyvim whichkey mappings

--- home-manager/neovim/lazyvim-plugins/editor.nix
+++ home-manager/neovim/lazyvim-plugins/editor.nix
@@ -21,30 +21,20 @@
         # this setting is independent of vim.opt.timeoutlen
         delay = 0;
         spec = [
-          {
-            __unkeyed-1 = "<leader>b";
-            group = "buffer";
-          }
-          {
-            __unkeyed-1 = "<leader>c";
-            group = "code";
-          }
-          {
-            __unkeyed-1 = "<leader>s";
-            group = "search";
-          }
-          {
-            __unkeyed-1 = "<leader>sn";
-            group = "noice";
-          }
-          {
-            __unkeyed-1 = "<leader>u";
-            group = "ui";
-          }
-          {
-            __unkeyed-1 = "<leader>x";
-            group = "diagnostics";
-          }
+          { __unkeyed-1 = "<leader>b"; group = "buffer"; }
+          { __unkeyed-1 = "<leader>c"; group = "code";}
+          #{ __unkeyed-1 = "<leader>d"; group = "debug";}
+          #{ __unkeyed-1 = "<leader>dp"; group = "profiler";}
+          #{ __unkeyed-1 = "<leader>f"; group = "file/find";}
+          { __unkeyed-1 = "<leader>s"; group = "search"; }
+          { __unkeyed-1 = "<leader>sn"; group = "noice"; }
+          { __unkeyed-1 = "<leader>u"; group = "ui"; }
+          { __unkeyed-1 = "<leader>x"; group = "diagnostics/quickfix"; }
+          { __unkeyed-1 = "["; group = "pref"; }
+          { __unkeyed-1 = "]"; group = "next"; }
+          { __unkeyed-1 = "g"; group = "goto"; }
+          { __unkeyed-1 = "gs"; group = "surround"; }
+          { __unkeyed-1 = "z"; group = "fold"; }
         ];
       };
     };
