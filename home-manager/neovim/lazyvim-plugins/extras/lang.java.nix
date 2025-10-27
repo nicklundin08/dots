c2bb6c6b4caee00e7b67beadad4bc16ee63cc90a
Nick Lundin <nicholas.lundin@betterlesson.com>
2025-10-26 19:56:54 -0600

    Neotest working but no standalone jar :/

--- home-manager/neovim/lazyvim-plugins/extras/lang.java.nix
+++ home-manager/neovim/lazyvim-plugins/extras/lang.java.nix
@@ -14,6 +14,17 @@
     LOMBOK_JAR_PATH = "${pkgs.lombok}/share/java/lombok.jar";
   };
 
+  let
+      myJar = pkgs.fetchurl {
+        url = "https://example.com/path/to/your/application.jar"; # Replace with your JAR URL
+        sha256 = "sha256-of-your-jar"; # Replace with the actual SHA256 hash
+      };
+    in
+    pkgs.writeScriptBin "run-my-app" ''
+      #!${pkgs.bash}/bin/bash
+      exec ${pkgs.jdk}/bin/java -jar ${myJar} "$@"
+    ''
+
   programs.nixvim = {
     extraPlugins = [
       (pkgs.vimUtils.buildVimPlugin {
@@ -25,6 +36,16 @@
           hash = "sha256-GNSxiiZmPdcU+bNSpnEnoGJ3q//bujxrRD65/V//2U8=";
         };
       })
+
+      # (pkgs.vimUtils.buildVimPlugin {
+      #   name = "nvim-jdtls";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "rcasia";
+      #     repo = "neotest-java";
+      #     rev = "fbdf5c3208007ccef1a8941e1229fc5f872c760e";
+      #     hash = "sha256-O1dhq6k8Y7sr9DRo3ER1Kc9N1l2LoELo/cvrMCUZZ4s=";
+      #   };
+      # })
     ];
 
     plugins.treesitter = {
@@ -38,10 +59,13 @@
     };
 
     plugins.neotest = {
-      settings.adapters = [
-        "gradle"
-        #"java"
-      ];
+      adapters.java = {
+        enable = true;
+
+        settings = {
+          ignore_wrapper = false;
+        };
+      };
     };
 
     plugins.conform-nvim = {
