8cc636648bcd91540d7034f8fb36d818b1c5a797
Nick Lundin <nicholas.lundin@betterlesson.com>
2025-11-05 08:24:29 -0700

    TS lsp working

--- home-manager/neovim/lazyvim-plugins/extras/lang.ts.nix
+++ home-manager/neovim/lazyvim-plugins/extras/lang.ts.nix
@@ -1,4 +1,29 @@
 {pkgs, ...}: {
   home.packages = [pkgs.vtsls];
-  programs.nixvim.plugins.lsp.servers.vtsls.enable = true;
+  programs.nixvim = {
+    plugins = {
+      lsp.servers.vtsls = {
+        enable = true;
+        filetypes = [
+          "javascript"
+          "javascriptreact"
+          "javascript.jsx"
+          "typescript"
+          "typescriptreact"
+          "typescript.tsx"
+        ];
+      };
+
+      treesitter = {
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+          javascript
+          jsdoc
+          json
+          jsonc
+          tsx
+          typescript
+        ];
+      };
+    };
+  };
 }
