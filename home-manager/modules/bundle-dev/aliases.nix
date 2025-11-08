82eafb2f4fc771d7c73a7800b55e350ed3317fa4
Nick Lundin <nick@moontower.net>
2025-11-08 09:39:22 -0700

    Added increment feature for home manager

--- home-manager/modules/bundle-dev/aliases.nix
+++ home-manager/modules/bundle-dev/aliases.nix
@@ -1,5 +1,16 @@
 {
   home.shellAliases = {
+    ########################################
+    # HM
+    ########################################
+    "hm.switch" = "git add . && home-manager switch --flake .";
+    "hm.show_host_for_nix" = "echo \"$(whoami)@$(hostname)\"";
+    "hm.show_host_for_git" = "echo \"hm/$(whoami)-$(hostname)\""; #git dont like @ symbols
+    "hm.create_branch_for_host" = "git branch $(hm.show_host_for_git)";
+
+    ## TODO: dont hardcode bash reload here?
+    "hm.increment" = "pre-commit && git commit -a && home-manager switch --flake . && git switch $(hm.show_host_for_git) && git reset --hard main && git switch main && exec bash";
+
     ssh_nix_tower = "ssh nick@10.0.0.134";
     show_ip = "ip -br addr show";
     nxs = "git add . && sudo nixos-rebuild switch --flake .";
