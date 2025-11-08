08a30256f266ab0394050d6cf7d1b9c8591878e5
Nick Lundin <nick@moontower.net>
2025-11-08 16:49:44 -0700

    Added increment ability for nixos

--- home-manager/modules/bundle-dev/aliases.nix
+++ home-manager/modules/bundle-dev/aliases.nix
@@ -1,7 +1,7 @@
 {
   home.shellAliases = {
     ########################################
-    # HM
+    # Home manager
     ########################################
     "hm.switch" = "git add . && home-manager switch --flake .";
     "hm.show_host_for_nix" = "echo \"$(whoami)@$(hostname)\"";
@@ -11,10 +11,22 @@
     ## TODO: dont hardcode bash reload here?
     "hm.increment" = "pre-commit && git commit -a && home-manager switch --flake . && git switch $(hm.show_host_for_git) && git reset --hard main && git switch main && exec bash";
 
+    ########################################
+    # Nix
+    ########################################
+    "nix.switch" = "git add . && sudo nixos-rebuild switch --flake .";
+    "nix.show_host_for_nix" = "echo \"$(whoami)@$(hostname)\"";
+    "nix.show_host_for_git" = "echo \"nix/$(whoami)-$(hostname)\""; #git dont like @ symbols
+    "nix.create_branch_for_host" = "git branch $(nix.show_host_for_git)";
+
+    ## TODO: dont hardcode bash reload here?
+    "nix.increment" = "pre-commit && git commit -a && nix.switch && git switch $(nix.show_host_for_git) && git reset --hard main && git switch main && exec bash";
+
+    ########################################
+    # MISC
+    ########################################
     ssh_nix_tower = "ssh nick@10.0.0.134";
     show_ip = "ip -br addr show";
-    nxs = "git add . && sudo nixos-rebuild switch --flake .";
-    hms = "git add . && home-manager switch --flake .";
     tux = "tmuxp load .";
     ns = "echo \"enter search term\"; read variable_name; echo \"$variable_name\" | xargs nix-search {} --json | jq -s | jq '.[].package_pname' | fzf --bind 'enter:become(nix-shell -p {})'";
     nsh = "nix-search --help";
