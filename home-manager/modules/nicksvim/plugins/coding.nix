37847fb01fc685299aeac8ed62e6fd576b7445b4
Nick Lundin <nick@moontower.net>
2025-11-11 08:17:07 -0700

    feat(cmp): better mappings for nvim cmp

--- home-manager/modules/nicksvim/plugins/coding.nix
+++ home-manager/modules/nicksvim/plugins/coding.nix
@@ -23,6 +23,24 @@
       #{ name = "path"; }
       #{ name = "buffer"; }
     ];
+    settings. mapping = {
+      # "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+      # "<C-f>" = "cmp.mapping.scroll_docs(4)";
+      "<C-Space>" = "cmp.mapping.complete()";
+      "<C-e>" = "cmp.mapping.close()";
+      "<C-n>" = "cmp.mapping.select_next_item()";
+      "<C-p>" = "cmp.mapping.select_prev_item()";
+      "<CR>" = "cmp.mapping.confirm({ select = true })";
+      # "<C-n>" = {
+      #   modes = ["i" "s"];
+      #   action = "cmp.mapping.select_next_item()";
+      # };
+      # "<C-p>" = {
+      #   modes = ["i" "s"];
+      #   action = "cmp.mapping.select_prev_item()";
+      # };
+      # "<CR>" = "cmp.mapping.confirm({ select = true })";
+    };
   };
 
   programs.nixvim.plugins.cmp-nvim-lsp.enable = true;
