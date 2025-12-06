216b54af95de3f38b7d6f91768c9095cb40cc87e
Nick Lundin <nick@moontower.net>
2025-12-06 11:27:37 -0700

    fix(stackbuilder.vite): remove duplicate path portion for vite app

--- home-manager/modules/stackbuilder/add-vite-app/default.nix
+++ home-manager/modules/stackbuilder/add-vite-app/default.nix
@@ -1,10 +1,7 @@
 {config, ...}: {
   home.shellAliases = {
     "stackbuilder.add_vite_app" = ''
-      mkdir -p src.app/ || true
-      cd src.app/
       npm create vite@latest src.app -- --template react-ts
-      cd ..
     '';
   };
 }
