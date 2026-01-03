269f0b3b77114e2a70b9eae22f5f94c7c6ac568e
Nick Lundin <nick@moontower.net>
2026-01-03 10:09:34 -0700

    fix(nicksvim): updated build process to generate the symlink (impure?)

--- home-manager/modules/nicksvim/default.nix
+++ home-manager/modules/nicksvim/default.nix
@@ -1,6 +1,18 @@
-{config, ...}: {
+{
+  lib,
+  config,
+  ...
+}: let
+  SYMLINK_TARGET = "~/dots/home-manager/modules/nicksvim/lua";
+in {
   # TODO: I just manually symlinked this for now. Should find a more pure way to do this? IDK...
   #home.file.".config/nicksvim/".source = config.lib.file.mkOutOfStoreSymlink ./lua;
   # look into https://search.nixos.org/options?channel=unstable&show=system.activationScripts&query=system.activation
   home.shellAliases."nicksvim" = "NVIM_APPNAME=nicksvim nvim";
+  home.activation = {
+    symlinkNvimDots = lib.hm.dag.entryAfter ["writeBoundary"] ''
+             echo linking ${SYMLINK_TARGET}
+      run ln -sfn ${SYMLINK_TARGET} ~/.config/nicksvim
+    '';
+  };
 }
