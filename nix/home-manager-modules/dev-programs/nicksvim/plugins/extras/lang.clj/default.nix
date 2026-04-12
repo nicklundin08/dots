9989333b73a7f6babf4f1a2834a136196ff61861
Nick Lundin <nick@moontower.net>
2026-04-11 18:40:29 -0600

    feat(nixvim.clj): added repl support via conjure

--- nix/home-manager-modules/dev-programs/nicksvim/plugins/extras/lang.clj/default.nix
+++ nix/home-manager-modules/dev-programs/nicksvim/plugins/extras/lang.clj/default.nix
@@ -2,8 +2,15 @@
   home.packages = [
     pkgs.cljfmt
   ];
+
+  xdg.configFile."clojure/deps.edn".source = ./deps.edn;
+  xdg.configFile.".clojure/deps.edn".source = ./deps.edn;
+
   programs.nixvim = {
     plugins = {
+      conjure = {
+        enable = true;
+      };
       treesitter = {
         grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
           clojure
