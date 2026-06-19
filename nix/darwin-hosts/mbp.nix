eb13e1a9bc2bda4d83927d5b8564f0ff4462d2a9
Nick Lundin <nlundin@armlend.com>
2026-06-19 16:43:01 -0600

    feat(macbookpro): added darwin host for mbp

---
+++ nix/darwin-hosts/mbp.nix
@@ -0,0 +1,51 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  pkgs,
+  inputs,
+  outputs,
+  ...
+}: {
+  imports = [
+    inputs.home-manager.darwinModules.home-manager
+  ];
+  environment.systemPackages = [
+    # pkgs.sl
+  ];
+  # programs.firefox.enable = true;
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs outputs;};
+    users = {
+      # Import your home-manager configuration
+      nick = import ../home-manager-hosts/home.work.nix;
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nick = {
+    home = "/Users/nick";
+    # isNormalUser = true;
+    description = "Nick";
+    # extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs; [
+      # wireguard-tools
+    ];
+  };
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Enable alternative shell support in nix-darwin.
+  # programs.fish.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = outputs.self.rev or outputs.self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
