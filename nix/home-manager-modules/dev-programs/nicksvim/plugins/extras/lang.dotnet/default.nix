8c2793740150e90390f6ca27771cb85fd7145d05
Nick Lundin <nlundin@armlend.com>
2026-06-20 14:51:26 -0600

    wip - lsp working!

--- nix/home-manager-modules/dev-programs/nicksvim/plugins/extras/lang.dotnet/default.nix
+++ nix/home-manager-modules/dev-programs/nicksvim/plugins/extras/lang.dotnet/default.nix
@@ -4,18 +4,58 @@
   ];
 
   programs.nixvim = {
+    extraConfigLua = ''
+      vim.lsp.config('omnisharp', {
+        cmd = {
+          'OmniSharp',
+          '--languageserver',
+          '--hostPID',
+          tostring(vim.fn.getpid()),
+        },
+      })
+    '';
     plugins = {
       lsp.servers.omnisharp = {
-       # run this to start
-       # :lua print(vim.inspect(vim.lsp.get_clients()))
+        # :LspInfo
+        # :LspLog
+        # :lua vim.lsp.set_log_level("debug")
+
+        # Force start
+        # :lua vim.lsp.enable("omnisharp")
+
+        # Check if enabled
+        # :lua print(vim.inspect(vim.lsp.is_enabled("omnisharp")))
+
+        # :lua print(vim.inspect(vim.lsp.get_clients()))
+        # :lua require("lspconfig").omnisharp.manager.try_add()
+        # :lua vim.lsp.start({name = "omnisharp",cmd = {"omnisharp"},root_dir = vim.fn.getcwd()})
+        # :lua print(vim.inspect(vim.lsp.start({name = "omnisharp",cmd = {"OmniSharp"},root_dir = vim.fn.getcwd()})))
+        # :lua print(vim.lsp.get_client_by_id(1).initialized)
+        # :lua print(vim.inspect(vim.lsp.get_client_by_id(1)))
+        # :lua print(vim.inspect(vim.lsp.get_client_by_id(1)))
+
+        # :lua print(vim.inspect(vim.lsp.config.omnisharp.root_dir(vim.fn.expand("%:p"))))
+        # :lua print(vim.inspect(vim.lsp.get_client_by_id(1)))
+
         enable = true;
-        filetypes = [
-          "cs"
-          "sln"
-          "csproj"
+        # filetypes = [
+        #   "cs"
+        #   "sln"
+        #   "csproj"
+        # ];
+        rootMarkers = [
+          ".git"
         ];
       };
 
+      # neotest = {
+      #   adapters.dotnet = {
+      #     enable = true;
+      #
+      #     settings = {
+      #     };
+      #   };
+      # };
       treesitter = {
         grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
           c_sharp
