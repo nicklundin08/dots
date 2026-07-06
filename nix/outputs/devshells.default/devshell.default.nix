513047f57574518787b6f90705a695d6a53709e1
Nick Lundin <nlundin@armlend.com>
2026-07-06 16:49:16 -0500

    wip

--- nix/outputs/devshells.default/devshell.default.nix
+++ nix/outputs/devshells.default/devshell.default.nix
@@ -1,7 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   packages = with pkgs; [
-    cowsay
+    git
+    lazygit
+    # fd-find
+    curl
+    ripgrep
+    gcc
+    # tree-sitter-cli
+    neovim
   ];
+
   shellHook = ''
     echo "Hello from shell hook!"
   '';
