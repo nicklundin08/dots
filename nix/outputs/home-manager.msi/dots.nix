0aac71d6d03de1ae26ee327c2ca99d036b9f0e56
Nick Lundin <nick@moontower.net>
2026-07-18 22:08:27 -0600

    refactor(msi): move some dots code to the distrobox file

--- nix/outputs/home-manager.msi/dots.nix
+++ nix/outputs/home-manager.msi/dots.nix
@@ -1,15 +1,9 @@
 {pkgs, ...}: let
-  pathPart1 = "/home/nick/.nix-profile/bin";
-  pathPart2 = "/nix/var/nix/profiles/default/bin";
   dotsName = "hm-msi";
 in {
   home.sessionVariables = {
     DOTS_BRANCH_NAME = dotsName;
   };
-  programs.bash.profileExtra = ''
-    [[ ":$PATH:" == *":${pathPart1}:"* ]] || export PATH="${pathPart1}:$PATH"
-    [[ ":$PATH:" == *":${pathPart2}:"* ]] || export PATH="${pathPart2}:$PATH"
-  '';
   home.packages = with pkgs; [
     (writeShellScriptBin "dots:rebuild" ''
       nix run home-manager/master -- switch -b backup --flake .#${dotsName}
--- nix/outputs/home-manager.msi/home.msi.nix
+++ nix/outputs/home-manager.msi/home.msi.nix
@@ -1,4 +1,8 @@
-{outputs, ...}: {
+{
+  outputs,
+  pkgs,
+  ...
+}: {
   imports = [
     ./dots.nix
     ./distrobox-workarounds.nix
@@ -32,7 +36,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [xremap];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
