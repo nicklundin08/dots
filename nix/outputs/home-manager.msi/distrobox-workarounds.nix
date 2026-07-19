31554078c1c623778e7791b09bbd8d0f66fbf5df
Nick Lundin <nick@moontower.net>
2026-07-19 09:27:21 -0600

    fix(distrobox): fixed profile path issue

--- nix/outputs/home-manager.msi/distrobox-workarounds.nix
+++ nix/outputs/home-manager.msi/distrobox-workarounds.nix
@@ -1,12 +1,15 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  pathPart1 = "/home/nick/.nix-profile/bin";
+  pathPart2 = "/nix/var/nix/profiles/default/bin";
+in {
   home.sessionVariables = {
     DISTROBOX_NIX_PATH = "123";
     NIX_CONFIG = "extra-experimental-features = flakes nix-command";
   };
-  # home.sessionPath = [
-  #   "/home/nick/.nix-profile/bin"
-  #   "/nix/var/nix/profiles/default/bin"
-  # ];
+  programs.bash.profileExtra = ''
+    [[ ":$PATH:" == *":${pathPart1}:"* ]] || export PATH="${pathPart1}:$PATH"
+    [[ ":$PATH:" == *":${pathPart2}:"* ]] || export PATH="${pathPart2}:$PATH"
+  '';
   home.packages = with pkgs; [
     (writeShellScriptBin "dots:start-daemon" ''
       sudo /nix/var/nix/profiles/default/bin/nix-daemon &
