52f2ca8b9c33dfc9f5e7954b7fac2bdc0bf7cdc6
Nick Lundin <nick@moontower.net>
2026-07-30 18:51:51 -0600

    docs(distrobox): added comment explaining distrobox podman symlink

--- nix/outputs/home-manager.msi/distrobox-workarounds.nix
+++ nix/outputs/home-manager.msi/distrobox-workarounds.nix
@@ -2,6 +2,11 @@
   pathPart1 = "/home/nick/.nix-profile/bin";
   pathPart2 = "/nix/var/nix/profiles/default/bin";
 in {
+  # Podman setup (manual)
+  # sudo ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman
+  # minikube config set driver podman
+  # minikube config set rootless true
+  # minikube config set container-runtime containerd
   home.sessionVariables = {
     DISTROBOX_NIX_PATH = "123";
     NIX_CONFIG = "extra-experimental-features = flakes nix-command";
