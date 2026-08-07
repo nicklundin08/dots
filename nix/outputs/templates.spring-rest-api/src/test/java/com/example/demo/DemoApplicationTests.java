073d30d89bde432d856bbf66ecb3a220535b4c3c
Nick Lundin <nick@moontower.net>
2026-08-06 21:43:35 -0600

    feat(templates/spring-rest-api): spotless

--- nix/outputs/templates.spring-rest-api/src/test/java/com/example/demo/DemoApplicationTests.java
+++ nix/outputs/templates.spring-rest-api/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,14 +2,11 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-//@Import(TestcontainersConfiguration.class)
+// @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
