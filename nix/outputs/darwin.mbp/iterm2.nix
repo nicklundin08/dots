affd301a8c800f7cd127c93ebdbd1971282ed72a
Nick Lundin <nick@moontower.net>
2026-09-17 13:02:35 -0600

    iterm2

--- nix/outputs/darwin.mbp/iterm2.nix
+++ nix/outputs/darwin.mbp/iterm2.nix
@@ -1,3 +1,22 @@
 {pkgs, ...}: {
+  ############################################################
+  # Global settings
+  ############################################################
+  # System settings -> keyboard -> shortcuts -> modifier keys
+  # caps -> cmd
+  # control -> caps
+  # option -> ctrl
+  # cmd -> option
+
+  ############################################################
+  # Iterm2 settings
+  ############################################################
+  # Settings -> profiles -> keys ->
+  # left/right option key -> dropdown -> escape+
+
+  # Settings -> Keys -> Remap modifiers
+  # Left/right control -> cmd
+  # Left/right cmd -> control
+
   home.packages = with pkgs; [iterm2];
 }
