e8d2a3762df7cdc7ee4ee7cc49456004f264894e
Nick Lundin <nick@moontower.net>
2026-09-17 13:02:31 -0600

    ollama -> codex

--- nix/outputs/darwin.mbp/agent.nix
+++ nix/outputs/darwin.mbp/agent.nix
@@ -1,10 +1,5 @@
 {pkgs, ...}: {
   environment.systemPackages = [
-    pkgs.openclaw
-    pkgs.ollama
-  ];
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "openclaw-2026.5.7"
+    pkgs.codex
   ];
 }
